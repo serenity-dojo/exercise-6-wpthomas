bb5808ad777e4af1d2be8af97318ac72cc53b68b
wpthomas <31428486+wpthomas@users.noreply.github.com>
2024-03-18 10:55:48 -0400

    Added Pet. Made Cat and Dog extend Pet. Added conditions to Feeder. Added Hamster class. Added required tests to WhenFeedingTheAnimals. All tests pass.

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,14 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
+        if (animal.equals("Cat")) {
+            return isPremium ? "Salmon" : "Tuna";
+        } else if (animal.equals("Dog")) {
+            return isPremium ? "Deluxe Dog Food" : "Dog Food";
+        } else if (animal.equals("Hamster")) {
+            return isPremium ? "Lettuce" : "Cabbage";
+        }
+
         return "Food";
     }
 }
